1411a3e0b9f4abbb3ef709dbaed8e8fd6aa1ccaa
Neylton Arantes <neylton.arantes@gmail.com>
2023-10-31 15:03:09 -0300

    Initial commit

---
+++ src/main/java/com/neylton/TodoAppCoreApplication.java
@@ -0,0 +1,13 @@
+package com.neylton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neylton/TodoAppCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neylton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
