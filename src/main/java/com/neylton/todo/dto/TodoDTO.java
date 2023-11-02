549fafd2cde55151a19823d43aee291f879454ec
Neylton Arantes <neylton.arantes@gmail.com>
2023-11-02 10:48:55 -0300

    Little adjustment on TodoDTO

--- src/main/java/com/neylton/todo/dto/TodoDTO.java
+++ src/main/java/com/neylton/todo/dto/TodoDTO.java
@@ -14,14 +14,14 @@ public class TodoDTO {
     private long id;
 
     @NotNull(message = "Nome da tarefa não pode ser um valor nulo.")
-    @NotBlank(message = "Foneça um nome de tarefa com pelo menos 03 caracteres.")
+    @NotBlank(message = "Forneça um nome de tarefa com pelo menos 03 caracteres.")
     private String todoName;
 
     private String description;
 
     @NotNull(message = "Prioridade da tarefa não pode ser um valor nulo.")
     @Min(value = 0, message = "Prioridade deve ser maior que zero.")
-    @Max(value = 5, message = "Prioridade deve ser menor que 5.")
+    @Max(value = 5, message = "Prioridade deve ser menor até 5.")
     private int priority;
 
     @NotNull(message = "Informe se a tarefa está ou não realizada.")
